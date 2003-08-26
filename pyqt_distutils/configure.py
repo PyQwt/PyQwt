7ae00b7b18e98e98d1cc463c313b564acb93ff3a
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2003-08-26 20:33:52 +0000

    Support Qt-3.2.1 snapshots

--- pyqt_distutils/configure.py
+++ pyqt_distutils/configure.py
@@ -728,6 +728,7 @@ def get_sip_t_options(qt_version_str, os_name):
         '3.1.1': 'Qt_3_1_1',
         '3.1.2': 'Qt_3_1_2',
         '3.2.0': 'Qt_3_2_0',
+        '3.2.1': 'Qt_3_2_0', # true for snapshot 2003-08-25
         }.get(qt_version_str)
 
     if not qt:
