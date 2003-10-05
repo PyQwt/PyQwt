503fbc93dde7ebae047a6ff9e109f8d66f515826
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2003-10-05 15:05:35 +0000

    By default Python-2.3 and PyQt/sip-3.8

--- win-pyqt.bat
+++ win-pyqt.bat
@@ -1,17 +1,17 @@
-REM Batch file to build PyQt-win-nc-3.5, -3.6 or 3.7.
-REM Put this file in PyQt-win-nc-3.5, -3.6 or 3.7 and run it.
+REM Batch file to build PyQt-win-nc-3.5, -3.6, -3.7 or -3.8.
+REM Put this file in PyQt-win-nc-3.5, -3.6, -3.7 or 3.8 and run it.
 REM You may have to adapt it for your Python installation.
 
 python build.py
 nmake
 
-REM Doing 'nmake' has put all library files in C:\python22\Lib\site-packages
+REM Doing 'nmake' has put all library files in C:\python23\Lib\site-packages
 REM Doing 'nmake install' may delete some necessary library files.
 REM Copying the *.py files leaves the *.{exp,lib,pyd} files in place.
-copy /y qt\qt.py C:\python22\lib\site-packages
-copy /y qtcanvas\qtcanvas.py C:\python22\Lib\site-packages
-copy /y qtgl\qtgl.py C:\python22\Lib\site-packages
-copy /y qtnetwork\qtnetwork.py C:\python22\Lib\site-packages
-copy /y qttable\qttable.py C:\python22\Lib\site-packages
-copy /y qtxml\qtxml.py C:\Python22\Lib\site-packages
+copy /y qt\qt.py C:\python23\lib\site-packages
+copy /y qtcanvas\qtcanvas.py C:\python23\Lib\site-packages
+copy /y qtgl\qtgl.py C:\python23\Lib\site-packages
+copy /y qtnetwork\qtnetwork.py C:\python23\Lib\site-packages
+copy /y qttable\qttable.py C:\python23\Lib\site-packages
+copy /y qtxml\qtxml.py C:\Python23\Lib\site-packages
 REM Add more copy commands, if appropriate
--- win-sip.bat
+++ win-sip.bat
@@ -1,5 +1,5 @@
-REM Batch file to build sip-win-nc-3.5, -3.6 or 3.7.
-REM Put this file in sip-win-nc-3.5, -3.6 or -3.7 and run it.
+REM Batch file to build sip-win-nc-3.5, -3.6, -3.7 or -3.8.
+REM Put this file in sip-win-nc-3.5, -3.6, -3.7 or -3.8 and run it.
 REM You may have to adapt it for your Python installation
 
 python build.py
@@ -7,6 +7,6 @@ nmake
 
 REM siplib\Makefile may be broken - copy files manually.
 cd siplib
-copy /y sip.h C:\python22\include
-copy /y sipQt.h C:\python22\include
+copy /y sip.h C:\python23\include
+copy /y sipQt.h C:\python23\include
 cd ..
