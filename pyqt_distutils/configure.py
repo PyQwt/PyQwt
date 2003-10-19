671d194119eb08e4e7bcbe5d2c46f3368fae216f
Gerard Vermeulen <gvermeul@users.sourceforge.net>
2003-10-19 14:23:41 +0000

    Support for Qt-3.2.2

--- pyqt_distutils/configure.py
+++ pyqt_distutils/configure.py
@@ -729,6 +729,7 @@ def get_sip_t_options(qt_version_str, os_name):
         '3.1.2': 'Qt_3_1_2',
         '3.2.0': 'Qt_3_2_0',
         '3.2.1': 'Qt_3_2_0',
+        '3.2.2': 'Qt_3_2_0',
         }.get(qt_version_str)
 
     if not qt:
